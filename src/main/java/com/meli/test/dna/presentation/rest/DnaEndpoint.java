b519b897fc0dae3c5cbab03d90bffeb415a7b603
juliannaPeace <julianna@blubybs2.com>
2021-08-02 09:17:56 -0300

    included dependency Swagger

--- src/main/java/com/meli/test/dna/presentation/rest/DnaEndpoint.java
+++ src/main/java/com/meli/test/dna/presentation/rest/DnaEndpoint.java
@@ -5,6 +5,9 @@ import com.meli.test.dna.core.domain.model.Dna;
 import com.meli.test.dna.presentation.api.DnaApi;
 import com.meli.test.dna.presentation.api.dto.ProcessDnaRequestDto;
 import com.meli.test.dna.presentation.api.dto.ProcessDnaResponseDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +22,12 @@ import javax.validation.Valid;
 public class DnaEndpoint {
     private final DnaService dnaService;
 
+    @ApiOperation(value = "Retorna se um determinando DNA é de um simio ou de um humano")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna se um DNA pertence a um Simio ou a um Humano"),
+            @ApiResponse(code = 404, message = "Foi enviado um payload inválido"),
+            @ApiResponse(code = 500, message = "Ocorreu erro interno no servidor"),
+    })
     @PostMapping(DnaApi.URI_PROCESS_DNA)
     public ResponseEntity<ProcessDnaResponseDto> processDna(@Valid @RequestBody ProcessDnaRequestDto processDnaRequestDto) {
 
